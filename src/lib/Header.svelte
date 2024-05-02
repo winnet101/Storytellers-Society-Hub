a806bfea2347b10413657e9e07ce62011cf6b1b8
winnet101 <115325533+winnet101@users.noreply.github.com>
2024-05-01 19:54:13 -0700

    Init commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,37 @@
+<menu>
+  <a href="">About Us</a>
+  <a href="">Calendar</a>
+  <a href="">Resources</a>
+  <a href="">Contact us!</a>
+</menu>
+
+<style>
+  menu {
+    margin: 0;
+    padding-inline-start: 0;
+    display: flex;
+    justify-content: space-around;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #242424;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+    width: max-content;
+    padding: 10px;
+    width: 25%;
+
+    display: grid;
+    justify-items: center;
+
+    transition:
+      0.2s background-color,
+      0.2s text-shadow,
+      0.2s color;
+  }
+  a:hover {
+    background-color: black;
+    color: white;
+    text-shadow: 0 0 5px white;
+  }
+</style>
